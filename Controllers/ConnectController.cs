8152485f3d63b85d1ed120217db271cef9aee17f
Äkwav <xekwavx@gmail.com>
2022-05-17 19:59:10 +0200

    simplify usage for discord bot

--- Controllers/ConnectController.cs
+++ Controllers/ConnectController.cs
@@ -29,7 +29,7 @@ namespace Coflnet.Sky.McConnect.Controllers
         public async Task<ConnectionRequest> CreateConnection(string userId, string mcUuid)
         {
             var user = await GetOrCreateUser(userId);
-            return await connectService.AddNewRequest(user,mcUuid);
+            return await connectService.AddNewRequest(user, mcUuid);
         }
 
 
@@ -37,15 +37,38 @@ namespace Coflnet.Sky.McConnect.Controllers
         [Route("user/{userId}")]
         public Task<User> GetConnections(string userId)
         {
-            return GetOrCreateUser(userId);
+            return GetOrCreateUser(userId, true);
         }
 
-        private async Task<User> GetOrCreateUser(string userId)
+        /// <summary>
+        /// Get all users stored which may or may not have a connected account
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet]
+        [Route("users")]
+        public async Task<IEnumerable<User>> GetUsers(int amount = 1000, int offset = 0)
+        {
+            return await db.Users.Skip(offset).Take(amount).ToListAsync();
+        }
+        /// <summary>
+        /// Get all users stored which may or may not have a connected account
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet]
+        [Route("users/connected")]
+        public async Task<IEnumerable<User>> GetVerifiedUsers(int amount = 1000, int offset = 0)
+        {
+            return await db.Users.Where(u=>u.Accounts.Where(a=>a.Verified).Any()).Include(u => u.Accounts.Where(a=>a.Verified)).Skip(offset).Take(amount).ToListAsync();
+        }
+
+        private async Task<User> GetOrCreateUser(string userId, bool blockSave = false)
         {
             var user = await db.Users.Where(u => u.ExternalId == userId).Include(u => u.Accounts).FirstOrDefaultAsync();
             if (user == null)
             {
                 user = new User() { ExternalId = userId };
+                if (blockSave)
+                    return user;
                 db.Users.Add(user);
                 await db.SaveChangesAsync();
             }
@@ -57,15 +80,15 @@ namespace Coflnet.Sky.McConnect.Controllers
         [Route("minecraft/{mcUuid}")]
         public async Task<User> GetUser(string mcUuid)
         {
-            return await db.McIds.Where(id=>id.AccountUuid == mcUuid && id.Verified).OrderByDescending(m=>m.UpdatedAt).Select(id=>id.User).FirstOrDefaultAsync();
+            return await db.McIds.Where(id => id.AccountUuid == mcUuid && id.Verified).OrderByDescending(m => m.UpdatedAt).Select(id => id.User).FirstOrDefaultAsync();
         }
         [HttpPost]
         [Route("user/{userId}/verify")]
         public async Task GetUser(string userId, string mcUuid)
         {
             var user = await GetOrCreateUser(userId);
-            var req = await connectService.AddNewRequest(user,mcUuid);
-            var con = await db.McIds.Where(i=>i.User == user && i.AccountUuid == mcUuid).FirstAsync();
+            var req = await connectService.AddNewRequest(user, mcUuid);
+            var con = await db.McIds.Where(i => i.User == user && i.AccountUuid == mcUuid).FirstAsync();
             await connectService.ValidatedLink(con.Id);
         }
     }
